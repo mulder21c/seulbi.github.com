e33f4f039fe4d52bfe8ad06659a2aeddc1daf373
unknown <>
2017-03-14 01:44:31 +0900

    index.html 페이지에 common.js 파일 적용

---
+++ js/common.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+(function(){
+	function openSearchLayer(event){
+		var srchLayer = document.querySelector("#search-layer"),
+			clsBtn = srchLayer.querySelector(".search-btn-reset");
+		event = event || window.event;
+		event.preventDefault ? event.preventDefault() : event.returnValue = false;
+		srchLayer.style.display = "block";
+		clsBtn.addEventListener("click", closeSrchLayer, false);
+	}
+	function closeSrchLayer(event){
+		var srchLayer = document.querySelector("#search-layer");
+		event = event || window.event;
+		event.preventDefault ? event.preventDefault() : event.returnValue = false;
+		srchLayer.style.display = "none";
+		clsBtn.removeEventListener("click", closeSrchLayer, false);
+	}
+	var srchBtn = document.querySelector("#search-layer-open");
+	srchBtn.addEventListener("click", openSearchLayer, false);
+})();
